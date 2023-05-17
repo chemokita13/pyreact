ff7439dca9601db4a0c67414ec57c8cad0804939
chemokita13 <chemokita13@gmail.com>
2023-05-17 19:25:10 +0200

    checkpoint

--- src/app.py
+++ src/app.py
@@ -1,9 +1,9 @@
-# importo el módulo de pyscript que permite la manipulación del DOM
+# pyscript module to access DOM // importo el módulo de pyscript que permite la manipulación del DOM
 import pyscript as ps
-# esta clase será la encargada de instanciar la app
+# that class will be instanced // esta clase será la encargada de instanciar la app
 class App():
     def __init__(self):
         self.DOM = ps.Element('MainBox')
-    # este método será llamado
+    # that method will be called in main.py file // este método será llamado
     def run(self):
         self.DOM.write("Hello World!")
